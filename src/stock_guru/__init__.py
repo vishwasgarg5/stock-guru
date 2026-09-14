6a5765c63f4dcf7e5ef7feb3c037e1d998bc7f18
Vishwas Garg <vishwasgarg.ml@gmail.com>
2026-09-15 00:06:34 +0530

    Export final readiness report builder

--- src/stock_guru/__init__.py
+++ src/stock_guru/__init__.py
@@ -2,6 +2,7 @@ from .pipeline import Pipeline
 from .scheduling import next_session_date, previous_session_date, validate_trading_calendar
 from .calendar_io import load_trading_calendar
 from .artifact_validation import validate_model_artifact
+from .final_readiness import build_final_readiness_report
 
 __all__ = [
     "Pipeline",
@@ -10,4 +11,5 @@ __all__ = [
     "validate_trading_calendar",
     "load_trading_calendar",
     "validate_model_artifact",
+    "build_final_readiness_report",
 ]
