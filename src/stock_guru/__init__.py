02f271fcfa1f86641ddf884171b67bcf6d1d905a
Vishwas Garg <vishwasgarg.ml@gmail.com>
2026-09-15 00:00:22 +0530

    Export trading calendar loader

--- src/stock_guru/__init__.py
+++ src/stock_guru/__init__.py
@@ -1,5 +1,6 @@
 from .pipeline import Pipeline
 from .scheduling import next_session_date, previous_session_date, validate_trading_calendar
+from .calendar_io import load_trading_calendar
 from .artifact_validation import validate_model_artifact
 
 __all__ = [
@@ -7,5 +8,6 @@ __all__ = [
     "next_session_date",
     "previous_session_date",
     "validate_trading_calendar",
+    "load_trading_calendar",
     "validate_model_artifact",
 ]
